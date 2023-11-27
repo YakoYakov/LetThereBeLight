27bb20b0089dd341019bd8c1cfad5866ef40f5f8
Yako Yakov <yako.yakov@accedia.com>
2023-11-27 10:52:46 +0200

    Revisit the constants

--- LetThereBeLightApp/LetThereBeLightApp/Constants/Constants.cs
+++ LetThereBeLightApp/LetThereBeLightApp/Constants/Constants.cs
@@ -2,6 +2,18 @@
 {
     public class Constants
     {
-        public const string API_SERVICE_BASE_URL = "https://localhost:7072/api/SmartBuld";
+        private const string API_SERVICE_BASE_URL = "https://localhost:7072/api/SmartBuld";
+
+        public const string DISCOVERY_ENDPOINT = API_SERVICE_BASE_URL + "/discover-devices?timeOut={0}";
+
+        public const string TOGGLE_ENDPOINT = API_SERVICE_BASE_URL + "/toggle";
+
+        public const string CHANGE_BRIGHTNESS_ENDPOINT = API_SERVICE_BASE_URL + "/brightness";
+
+        public const string CHANGE_COLOR_TEMPERATURE_ENDPOINT = API_SERVICE_BASE_URL + "/color-temperature";
+
+        public const string CHANGE_RGB_ENDPOINT = API_SERVICE_BASE_URL + "/rgb";
+
+        public const string CHANGE_NAME_ENDPOINT = API_SERVICE_BASE_URL + "/name";
     }
 }
