098325af6cc72edb5a983a13c8b28adcf33a90fd
Yako Yakov <yako.p.yakov@gmail.com>
2023-11-27 13:54:50 +0200

    Simplify discovery service code

--- LetThereBeLightApi/LetThereBeLight.Services/DiscoveryService.cs
+++ LetThereBeLightApi/LetThereBeLight.Services/DiscoveryService.cs
@@ -16,145 +16,53 @@ namespace LetThereBeLight.Services
 
         public List<SmartBulb> Devices { get; set; } = new List<SmartBulb>();
 
-        
         public List<SmartBulb> DiscoverDevices(int SSDP_receiveTimeOut = SSDP_timeOut, int pingCount = 1, NetworkInterface? networkInterface = null)
         {
             Dictionary<IPAddress, SmartBulb> devices = new();
 
-            using (UdpClient socket = new())
+
+            using (var socket = new UdpClient())
             {
+                socket.Client.ReceiveTimeout = 1000;
+
                 IPAddress multicastAddress = IPAddress.Parse(multiCastAddress);
-                IPEndPoint multicastEndpoint = new(multicastAddress, SSDP_port);
 
-                bool isMulticastJoined = false;
+                var remoteEndPoint = new IPEndPoint(multicastAddress, SSDP_port);
+                var anyEndPoint = new IPEndPoint(IPAddress.Any, SSDP_port);
 
-                if (networkInterface == null)
-                {
-                    foreach (NetworkInterface currentNetworkInterface in NetworkInterface.GetAllNetworkInterfaces())
-                    {
-                        var tmpClient = socket.Client;
-                        isMulticastJoined = TryJoinMulticastAndSetListener(ref tmpClient, currentNetworkInterface, multicastEndpoint);
-                        socket.Client = tmpClient;
-
-                        //Break if this interface joined the Multicast
-                        if (isMulticastJoined)
-                        {
-                            networkInterface = currentNetworkInterface;
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    var tmpClient = socket.Client;
-                    isMulticastJoined = TryJoinMulticastAndSetListener(ref tmpClient, networkInterface, multicastEndpoint);
-                    socket.Client = tmpClient;
-                }
-
-                if (!isMulticastJoined)
-                {
-                    throw new Exception("Failed to join Multicast with the network interface.");
-                }
-
-                // Disable Multicast packet loop back
-                socket.Client.MulticastLoopback = false;
-
-                socket.Client.ReceiveTimeout = SSDP_receiveTimeOut;
+                socket.JoinMulticastGroup(multicastAddress);
 
                 byte[] buffer = Encoding.ASCII.GetBytes(SSDP_searchMessage);
+                socket.Send(buffer, buffer.Length, remoteEndPoint);
 
-                for (int i = 0; i < pingCount; i++)
+                while (true)
                 {
-                    socket.Send(buffer, buffer.Length, multicastEndpoint);
-
-                    while (true)
+                    try
                     {
-                        try
-                        {
-                            IPEndPoint? sourceEndPoint = default;
-                            byte[] message = socket.Receive(ref sourceEndPoint);
-                            IPAddress deviceIp = sourceEndPoint.Address;
+                        IPEndPoint? sourceEndPoint = default;
+                        byte[] message = socket.Receive(ref sourceEndPoint);
+                        IPAddress deviceIp = sourceEndPoint.Address;
 
-                            lock (devices)
-                            {
-                                if (devices.ContainsKey(deviceIp))
-                                {
-                                    continue;
-                                }
-                                var deviceInfo = Encoding.ASCII.GetString(message);
-                                var device = SmartBulb.Initialize(deviceInfo);
-                                devices.Add(deviceIp, device);
-                            }
-                        }
-                        catch
+                        lock (devices)
                         {
-                            break;
+                            if (devices.ContainsKey(deviceIp))
+                            {
+                                continue;
+                            }
+                            var deviceInfo = Encoding.ASCII.GetString(message);
+                            var device = SmartBulb.Initialize(deviceInfo);
+                            devices.Add(deviceIp, device);
                         }
                     }
-
+                    catch
+                    {
+                        break;
+                    }
                 }
             }
+
             Devices = devices.Select(n => n.Value).ToList();
             return Devices;
-        }
-
-        private static bool IsNicGoodForMulticast(NetworkInterface adapter)
-        {
-            if (!adapter.GetIPProperties().MulticastAddresses.Any())
-                return false; // most of VPN adapters will be skipped
-            if (!adapter.SupportsMulticast)
-                return false; // Doesn't support Multicast, no point in trying
-            if (OperationalStatus.Up != adapter.OperationalStatus)
-                return false; // This adapter is off or not connected
-            IPv4InterfaceProperties IPv4Props = adapter.GetIPProperties().GetIPv4Properties();
-            if (IPv4Props == null)
-                return false; // IPv4 is not configured on this adapter
-
-            // IPv4InterfaceProperties.Index's getter can throw if the interface has no index
-            try
-            {
-                _ = IPv4Props.Index;
-            }
-            catch
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private static bool TryJoinMulticastAndSetListener(ref Socket socket, NetworkInterface networkInterface, IPEndPoint multicastEndpoint)
-        {
-            if (!IsNicGoodForMulticast(networkInterface))
-            {
-                return false;
-            }
-
-            // Set incoming Multicast packet address, pick the first address that doesn't fail joining to the Multicast group
-            foreach (var localAddr in networkInterface.GetIPProperties().UnicastAddresses.Select(x => x.Address))
-            {
-                try
-                {
-                    socket.Bind(new IPEndPoint(localAddr, multicastEndpoint.Port));
-
-                    //Index getter shouldn't throw here because IsNicGoodForMulticast checks for that
-                    int ifIndex = networkInterface.GetIPProperties().GetIPv4Properties().Index;
-
-                    MulticastOption multicastOptions = new(multicastEndpoint.Address, ifIndex);
-                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, multicastOptions);
-
-                    // Set outgoing Multicast packet interface
-                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastInterface, (int)IPAddress.HostToNetworkOrder(ifIndex));
-
-                    return true;
-                }
-                catch
-                {
-                    continue;
-                }
-            }
-
-            return false;
         }
     }
 }
