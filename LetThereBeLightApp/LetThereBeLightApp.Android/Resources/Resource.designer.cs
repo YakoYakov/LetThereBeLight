274589610b917db6529064d6cfe71beec8cf6c2f
Yako Yakov <yako.yakov@accedia.com>
2023-12-05 11:39:45 +0200

    Added new lunch icon

--- LetThereBeLightApp/LetThereBeLightApp.Android/Resources/Resource.designer.cs
+++ LetThereBeLightApp/LetThereBeLightApp.Android/Resources/Resource.designer.cs
@@ -24876,14 +24876,23 @@ namespace LetThereBeLightApp.Droid
 		public partial class Mipmap
 		{
 			
+			// aapt resource value: 0x7F0C0003
+			public const int icon = 2131492867;
+			
+			// aapt resource value: 0x7F0C0004
+			public const int icon_round = 2131492868;
+			
 			// aapt resource value: 0x7F0C0000
-			public const int icon = 2131492864;
+			public const int ic_launcher = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
+			public const int ic_launcher_adaptive_back = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			public const int ic_launcher_adaptive_fore = 2131492866;
+			
+			// aapt resource value: 0x7F0C0005
+			public const int launcher_foreground = 2131492869;
 			
 			static Mipmap()
 			{
